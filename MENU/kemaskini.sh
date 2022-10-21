f976c5721965db643ce1c66101450ed7ec64e25d
JINGGOVPN <114797590+JinGGoVPN@users.noreply.github.com>
2022-10-21 19:17:06 +0800

    Update kemaskini.sh

--- MENU/kemaskini.sh
+++ MENU/kemaskini.sh
@@ -2,10 +2,12 @@
 clear
 #delete file
 rm -f /usr/local/bin/menu
+rm -f /usr/local/bin/backup
 
 # download script
 cd /usr/local/bin
 wget -O menu "https://raw.githubusercontent.com/jinggovpn/DATA/main/MENU/menu.sh" && chmod +x menu
+wget -O backup "https://raw.githubusercontent.com/jinggovpn/DATA/main/MENU/backup.sh" && chmod +x backup
 
 cd
 clear
