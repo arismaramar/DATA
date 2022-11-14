ed31a69d01b621084b6817299c06b51237d67e6d
JINGGOVPN <114797590+JinGGoVPN@users.noreply.github.com>
2022-11-15 02:08:59 +0800

    Update kemaskini.sh

--- MENU/kemaskini.sh
+++ MENU/kemaskini.sh
@@ -3,12 +3,14 @@ clear
 #delete file
 rm -f /usr/local/bin/trial
 rm -f /usr/local/bin/usernew
+rm -f /usr/local/bin/menu
 
 
 # download script
 cd /usr/local/bin
 wget -O trial "https://raw.githubusercontent.com/jinggovpn/DATA/main/SSHOVPN/trial.sh" && chmod +x trial
 wget -O usernew "https://raw.githubusercontent.com/jinggovpn/DATA/main/SSHOVPN/usernew.sh" && chmod +x usernew
+wget -O menu "https://raw.githubusercontent.com/jinggovpn/DATA/main/SSHOVPN/menu.sh" && chmod +x menu
 
 cd
 clear
